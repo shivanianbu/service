383816ff8bcf64cf4cc0bdd9a21198b2ea2a88c7
shivanianbu <43634125+shivanianbu@users.noreply.github.com>
2019-08-07 14:36:31 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/owner/add', 'OwnerController@add');
-Route::post('/owner/add', 'OwnerController@save');
-Route::post('/owner/add', 'OwnerController@store');
-Route::get('/owner', 'OwnerController@show');
-Route::get('/owner/{id}/view', 'OwnerController@view');
-Route::get('/owner/{id}/edit', 'OwnerController@edit');
-
-Route::post('/owner/{id}/edit', 'OwnerController@update');
-Route::get('/owner/{id}/delete', 'OwnerController@delete');
 
 
